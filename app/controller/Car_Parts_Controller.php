a015f64a25444c760dad1fd5b3d8b9f90ba7b7bd
n0nag0n <n0nag0n@sky-9.com>
2021-01-15 11:00:33 -0700

    updates

--- app/controller/Car_Parts_Controller.php
+++ app/controller/Car_Parts_Controller.php
@@ -2,16 +2,42 @@
 
 class Car_Parts_Controller {
 	public function index(Base $fw, array $args = []): void {
+		$Car_Part = new Car_Part($fw->DB);
+		$all_car_parts = $Car_Part->find([ 'name LIKE ?', '%seat%' ]);
 		echo '<pre>';
-		var_dump($fw->DB->exec("SELECT * FROM car_parts"));
+		foreach($all_car_parts as $car_part) {
+			echo "ID: {$car_part->id}\n";
+			echo "Part: {$car_part->name}\n";
+		}
 		echo '</pre>';
 	}
 
 	public function insert(Base $fw, array $args = []): void {
 		$car_part_name = $args['car_part_name'];
 
-		//$fw->get('DB')->exec("INSERT INTO car_parts (name) VALUES (?)")
-		$fw->get('DB')->exec("INSERT INTO car_parts SET name = ?", [ $car_part_name ]);
+		$Car_Part = new Car_Part($fw->DB);
+		$Car_Part->name = $car_part_name;
+		$Car_Part->save();
+		// $Car_Part->id;
+		$Car_Part->reset();
+
+		$Car_Part->name = $car_part_name.' Copy';
+		$Car_Part->save();
+
+		$fw->reroute('/car-parts', false);
+	}
+
+	public function update(Base $fw, array $args = []): void {
+		$updated_name = $args['updated_name'];
+		$id = $args['car_part_id'];
+
+		$Car_Part = new Car_Part($fw->DB);
+		$Car_Part->load([ 'id = ?', $id ]);
+
+		//$Car_Part->copyfrom('POST');
+
+		$Car_Part->name = $updated_name;
+		$Car_Part->save();
 
 		$fw->reroute('/car-parts', false);
 	}
